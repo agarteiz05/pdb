8661bebfbc532c87212ced9241c0f5cbf2723dfb
Aliana Sanchez <agarteiz05@gmail.com>
2026-08-18 14:45:17 -0400

    enter otra vezzzz

--- app/l/[code]/page.tsx
+++ app/l/[code]/page.tsx
@@ -1,6 +1,9 @@
 import { supabase } from "@/lib/supabase";
 import AchievementUnlock from "@/components/AchievementUnlock";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function LogroPage({
   params,
 }: {
